2627c3ea33750a728c2b59c5c6cd6b4bfaeaee4a
Acheao <273074720linchao@sina.com>
2018-05-15 23:15:21 +0800

    add coordinate change function

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+var canvas = c.getContext("2d");
 c.height = 600;
 c.width = 400;
 $("#tetris").width();
 $("#tetris").height();
-ctx.fillStyle="#BFBFBF";
+canvas.fillStyle="#BFBFBF";
 for(var x=0;x<10;x++){
     for(var y=0;y<15;y++){
-        ctx.fillRect(x*40,40*y,40,40);
+        canvas.fillRect(x*40,40*y,40,40);
 
     }
 }
-ctx.fillStyle="#FFFFFF";
+canvas.fillStyle="#FFFFFF";
 for(var x1=0;x1<10;x1++){
     for(var y1=0;y1<15;y1++){
-        ctx.fillRect(x1*40+5,y1*40+5,30,30);
+        canvas.fillRect(x1*40+5,y1*40+5,30,30);
     }
 }
-ctx.fillStyle="#BFBFBF";
+canvas.fillStyle="#BFBFBF";
 for(var x2=0;x2<10;x2++){
     for(var y2=0;y2<15;y2++){
-        ctx.fillRect(x2*40+10,y2*40+10,20,20);
+        canvas.fillRect(x2*40+10,y2*40+10,20,20);
     }
 }
+canvas.fillStyle="#4F4F4F";
+canvas.fillRect(0,0,40,40);
+canvas.fillRect(40,40,40,40);
+canvas.fillRect(40,0,40,40);
+canvas.fillRect(80,0,40,40);
 
-ctx.fillStyle="#4F4F4F";
-ctx.fillRect(0,0,40,40);
-ctx.fillRect(40,40,40,40);
-ctx.fillRect(40,0,40,40);
-ctx.fillRect(80,0,40,40);
-
-ctx.fillStyle="#FFFFFF";
-ctx.fillRect(5,5,30,30);
-ctx.fillRect(45,5,30,30);
-ctx.fillRect(85,5,30,30);
-ctx.fillRect(45,45,30,30);
+canvas.fillStyle="#FFFFFF";
+canvas.fillRect(5,5,30,30);
+canvas.fillRect(45,5,30,30);
+canvas.fillRect(85,5,30,30);
+canvas.fillRect(45,45,30,30);
 
 
-ctx.fillStyle="#4F4F4F";
-ctx.fillRect(10,10,20,20);
-ctx.fillRect(50,10,20,20);
-ctx.fillRect(90,10,20,20);
-ctx.fillRect(50,50,20,20);
+canvas.fillStyle="#4F4F4F";
+canvas.fillRect(10,10,20,20);
+canvas.fillRect(50,10,20,20);
+canvas.fillRect(90,10,20,20);
+canvas.fillRect(50,50,20,20);
 
 function coordinate(x,y) {
     var coordinate = new Object;
@@ -49,4 +48,41 @@ function coordinate(x,y) {
     coordinate.y=y;
     return coordinate;
 }
-var tes = coordinate(1,2);
+var tes = coordinate(6,7);
+//画黑色的方块drawing black Rect
+function drawingBlackRect(point) {
+    canvas.fillStyle="#4F4F4F";
+    canvas.fillRect(point.x*40,point.y*40,40,40);
+    canvas.fillStyle="#FFFFFF";
+    canvas.fillRect(point.x*40+5,point.y*40+5,30,30);
+    canvas.fillStyle="#4F4F4F";
+    canvas.fillRect(point.x*40+10,point.y*40+10,20,20);
+
+}
+//画灰色的方块drawing grey Rect
+function drawingGreyRect(point) {
+    canvas.fillStyle="#BFBFBF";
+    canvas.fillRect(point.x*40,point.y*40,40,40);
+    canvas.fillStyle="#FFFFFF";
+    canvas.fillRect(point.x*40+5,point.y*40+5,30,30);
+    canvas.fillStyle="#BFBFBF";
+    canvas.fillRect(point.x*40+10,point.y*40+10,20,20);
+
+}
+//以点A坐标为中心，顺时针转90度，返回该坐标
+function rotate(A,B) {
+    var pointC = coordinate(B.x-A.x,B.y-A.y);
+    var pointD = coordinate(-pointC.y,pointC.x);
+    var returnPoint = coordinate(pointD.x+A.x,pointD.y+A.y);
+     return returnPoint;
+}
+
+drawingGreyRect(tes);
+drawingBlackRect(tes);
+
+
+
+var testA = coordinate(5,10);
+var testB = coordinate(6,10);
+drawingBlackRect(testA);
+drawingBlackRect(testB);
