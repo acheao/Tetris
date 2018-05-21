c84ec60f443b6a2f9ebd03da8244238fb5d17c39
Acheao <273074720linchao@sina.com>
2018-05-21 23:03:22 +0800

    add shape.js file

---
+++ static/shape.js
@@ -0,0 +1,12 @@
+// 初始化形状
+// init shape
+var x = 3;
+var y = 0;
+var initPoint = coordinate(x,y);
+
+function produceShape(initPoint) {
+    Math.floor(Math.random()*10);
+
+}
+
+var zShape = new Array();
